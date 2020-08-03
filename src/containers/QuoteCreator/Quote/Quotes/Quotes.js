7643f27586075d7ba1f943ba8478cb66212d9f3a
Willis <jcp0524@gmail.com>
2020-08-03 13:30:13 +1200

    connected Quotes.js search bar

--- src/containers/QuoteCreator/Quote/Quotes/Quotes.js
+++ src/containers/QuoteCreator/Quote/Quotes/Quotes.js
@@ -4,13 +4,27 @@ import { connect } from 'react-redux';
 import Spinner from '../../../../components/UI/Spinner/Spinner';
 import * as actionCreators from '../../../../store/actions/index';
 import classes from './Quotes.module.css';
+import QuotesStatusFilter from './QuotesStatusFilter';
 
 class Quotes extends Component {
     state = {
         viewingQuote: false,
         selectedQuoteKey: null,
         propsLocationKey: null,
-        quotesArray: []
+        quotesArray: [],
+        keyValueQuotesArray: [],
+        status: {
+            Quote: {
+                created: false,
+                sent: false,
+                accepted: false
+            },
+            Invoice: {
+                created: false,
+                sent: false,
+                paid: false
+            }
+        },
     }
 
     componentDidMount () {
@@ -20,7 +34,6 @@ class Quotes extends Component {
             this.props.onResetQuote();
             this.props.onFetchQuotes();
         }
-        
     }
     componentDidUpdate () {
         if (this.props.quoteSubmitted) { // refetching updated quotes array after editing quote has been submitted or quote deleted
@@ -36,6 +49,22 @@ class Quotes extends Component {
             this.setState({ viewingQuote : false, selectedQuoteKey : null, propsLocationKey : this.props.location.key})
             this.props.onFetchQuotes()
         }
+
+        if (this.props.quotesFetched && this.state.keyValueQuotesArray.length === 0) { // initializes this.state.keyValueQuotesArray
+            this.initQuotesHandler()
+        }
+
+    }
+
+    initQuotesHandler = () => { // initializes key to each quote in quotesArray
+        let quotesArray = [];
+        for (let quote in this.props.quotes) {
+            quotesArray.push({
+                key: this.props.quotes[quote].id,
+                data: this.props.quotes[quote]
+            })
+        }
+        this.setState({ keyValueQuotesArray : quotesArray })
     }
 
     viewQuoteHandler = (quote) => {
@@ -50,22 +79,37 @@ class Quotes extends Component {
     }
 
     handleChange = (event) => {
-        let currentList = [];
-        let quotesArray = [];
-        for (let quote in this.props.quotes) {
-            quotesArray.push({
-                key: this.props.quotes[quote].id,
-                data: this.props.quotes[quote]})
-        }
+        let currentList = [
+            ...this.state.keyValueQuotesArray
+        ]
+        let matches = []
 
         if (event.target.value !== "") {
-            currentList = quotesArray;
-            let matches = currentList.filter(value => {
+            matches = currentList.filter(value => {
                 return (
-                    value.data.client.company.toLowerCase().includes(event.target.value)
+                    value.data.client.company.toLowerCase().includes(event.target.value) ||
+                    value.data.reference.quoteUnit.toLowerCase().includes(event.target.value) ||
+                    value.data.reference.quoteReference.toLowerCase().includes(event.target.value) ||
+                    value.data.reference.clientReference.toLowerCase().includes(event.target.value)
                 )
             })
         }
+        this.setState({ keyValueQuotesArray : matches})
+    }
+
+    statusChangeHandler = (event) => {
+        let str = event.target.id.split(' ');
+        console.log(str)
+        console.log(event.target.checked)
+        let stateStatusCopy = {
+            ...this.state.status,
+            [str[0]]: {
+                ...this.state.status[str[0]],
+                [str[1]]: event.target.checked
+            }
+        }
+        console.log(stateStatusCopy)
+        this.setState({ status : stateStatusCopy })
     }
     
     render () {
@@ -77,22 +121,27 @@ class Quotes extends Component {
         )
         let heading = <h2 className={classes.Heading}>Quotes</h2>
         
-        let quotesArray = [];
-        for (let quote in this.props.quotes) {
-            quotesArray.push({
-                key: this.props.quotes[quote].id,
-                data: this.props.quotes[quote]})
-        }
+        let quotesHeader = (
+            <div className={classes.QuoteHeader}>
+                <p className={classes.Element}>Client</p>
+                <p className={classes.Element}>Reference</p>
+                <p className={classes.Element}>Client Reference</p>
+                <p className={classes.Element}>Quote Unit</p>
+                <p className={classes.Element}>$</p>
+            </div>
+        )
+
         let quotes = <Spinner />
 
         if(!this.props.loading && !this.state.viewingQuote && this.state.quotesArray === this.props.quotes) {
-            quotes = quotesArray.map((quote) => {
+            quotes = this.state.keyValueQuotesArray.map((quote) => {
                 return (
                     <div key={quote.key} className={classes.Quote} onClick={() => this.viewQuoteHandler(quote)}>
-                        <p className={classes.Element}>Client: {quote.data.client.company}</p>
-                        <p className={classes.Element}>Reference: {quote.data.reference.quoteReference}</p>
-                        <p className={classes.Element}>Client Reference: {quote.data.reference.clientReference}</p>
-                        <p className={classes.Element}>Quote Unit: {quote.data.reference.quoteUnit}</p>
+                        <p className={classes.Element}>{quote.data.client.company}</p>
+                        <p className={classes.Element}>{quote.data.reference.quoteReference}</p>
+                        <p className={classes.Element}>{quote.data.reference.clientReference}</p>
+                        <p className={classes.Element}>{quote.data.reference.quoteUnit}</p>
+                        <p className={classes.Element}>{quote.data.price}</p>
                     </div>
                 )
             })
@@ -101,7 +150,12 @@ class Quotes extends Component {
         return (
             <div className={classes.Quotes}>
                 {search}
+                <QuotesStatusFilter 
+                    status={this.state.status}
+                    onStatusChange={this.statusChangeHandler}
+                />
                 {heading}
+                {quotesHeader}
                 {quotes}
             </div>
         )
